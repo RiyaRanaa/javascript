7a30bf296329fedc6980dfb200c3eb534e9a268c
Riya Rana <114066181+RiyaRanaa@users.noreply.github.com>
2024-06-28 07:20:07 +0000

    03datatype

---
+++ basic/03datatype.js
@@ -0,0 +1,27 @@
+"use strict";//treat all JS code as newer version
+// alert(3+3) // we are using nodejs, not browser
+//react node - semicol avoid
+console.log(3+3);console.log("riya")//this line was correct but not readable
+console.log(3+3);
+console.log("riya") // code readability should be high
+//data types
+let name="Riya"//string datatype
+let age=22//numbers value
+let isLoggedIn=false//boolean datatype
+/*
+premitive datatype
+1. number datatype => 2^53 range
+2. bigint datatype => big value int
+3. String datatype => ""
+4. boolean datatype => true/false
+5. null datatype => standalone value, speacially null is tpye, representation of emty value
+6. undefined =>  values are not assigned 
+7. symbol => for uniqueness
+
+non-primitive
+1. object
+
+*/
+console.log(typeof"Riya")//typeof operator returns the data type of a JavaScript variable
+console.log(typeof null)//object type
+console.log(typeof undefined)//undefined type
