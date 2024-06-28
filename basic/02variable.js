0d5ea3ff221d802a40e4b98e40626d35f77b799a
Riya Rana <114066181+RiyaRanaa@users.noreply.github.com>
2024-06-28 06:53:39 +0000

    container config done

---
+++ basic/02variable.js
@@ -0,0 +1,10 @@
+const accId=12345
+let accEmail="riya@gmail.com"
+var accPassword="123"
+accCity="BBSR"
+console.log(accId);
+// accId=23 //not allowed bcoz 'const' variable is a work for constant, we can't change value of this 
+accEmail="rana@gmail.com"
+accPassword="932"
+accCity="Bangalore"
+console.table([accId,accEmail,accPassword,accCity])
